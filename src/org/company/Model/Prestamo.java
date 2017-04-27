7b6bf2c349d07d7e5bb62cb64f60d2982ea5f3db
Marco Borja <marcoabr93@gmail.com>
2017-04-27 18:54:00 +0200

    s

---
+++ src/org/company/Model/Prestamo.java
@@ -0,0 +1,7 @@
+package org.company.Model;
+
+/**
+ * Created by marco on 27/4/17.
+ */
+public interface Prestamo {
+}
---
+++ src/org/company/Model/Prestatario.java
@@ -0,0 +1,11 @@
+package org.company.Model;
+
+import java.util.Date;
+
+/**
+ * Created by marco on 27/4/17.
+ */
+public class Prestatario {
+    private Date fecha;
+    private int duracionPrestamo;
+}
